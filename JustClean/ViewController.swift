e4fc77281656bc5f2ca844fb2b64ef44aa238c6c
wuchang <wuchang@luminesence.jp>
2022-06-02 16:16:10 +0400

    Initial Commit

---
+++ JustClean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustClean
+//
+//  Created by wuchang on 2022/6/2
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
